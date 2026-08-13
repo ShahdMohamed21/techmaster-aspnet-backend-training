79cf501149ba19b515bc7f3b66b5fd8e48d2a8ed
shahd mohamed <shahddmohamed55@gmail.com>
2026-08-13 22:34:44 +0300

    add csharp drills 4-6

--- phase-01-backend-foundations/task-01-csharp-drills/Program.cs
+++ phase-01-backend-foundations/task-01-csharp-drills/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+
+
 namespace Task01
 {
     internal class Program
@@ -9,8 +11,8 @@ namespace Task01
             Console.WriteLine("====Drill 01 — Temperature Converter====");
             Console.WriteLine("Enter The Temperature : ");
             double temp;
-            bool IsConverted = double.TryParse(Console.ReadLine() , out temp);
-            if(IsConverted)
+            bool IsConverted = double.TryParse(Console.ReadLine(), out temp);
+            if (IsConverted)
             {
                 Console.WriteLine($"Fahrenheit : {temp * 9 / 5 + 32:F2} ");
 
@@ -24,8 +26,8 @@ namespace Task01
             Console.WriteLine("====Drill 02 — Grade Calculator====");
             Console.WriteLine("Enter Your Grade : ");
             int grade;
-            bool isconverted= int.TryParse(Console.ReadLine(),out grade);
-            if(isconverted)
+            bool isconverted = int.TryParse(Console.ReadLine(), out grade);
+            if (isconverted)
             {
                 if (grade < 0 || grade > 100)
                 {
@@ -54,8 +56,8 @@ namespace Task01
                         Console.WriteLine("F");
                     }
                 }
-                
-           
+
+
 
             }
             else
@@ -95,8 +97,128 @@ namespace Task01
                     Console.WriteLine("Account Locked");
                 }
             }
+            // Drill 04 — Even / Odd Analyzer
+            Console.WriteLine("====Drill 04 — Even / Odd Analyzer====");
+            Console.WriteLine("Enter The Number Of Numbers : ");
+            int count;
+            bool isCountValid = int.TryParse(Console.ReadLine(), out count);
+            if (!isCountValid || count <= 0)
+            {
+                Console.WriteLine("The Count Must Be Greater Than 0");
+            }
+            else
+            {
+
+
+                List<int> odd = new List<int>();
+                List<int> even = new List<int>();
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine($"Enter Number{i + 1}");
+                    int num;
+                    bool isNumberValid = int.TryParse(Console.ReadLine(), out num);
+                    if (!isNumberValid)
+                    {
+                        Console.WriteLine("Invalid Number");
+                        i--;
+                        continue;
+                    }
+                    if (num % 2 == 0)
+                    {
+                        even.Add(num);
+                    }
+                    else
+                    {
+                        odd.Add(num);
+                    }
+
+                }
+                Console.Write("Even | ");
+                foreach (var i in even)
+                {
+                    Console.Write($" {i} ");
+                }
+                Console.Write("Odd | ");
+                foreach (var i in odd)
+                {
+                    Console.Write($" {i} ");
+                }
+                Console.WriteLine();
+                Console.WriteLine($"Even Count: {even.Count}");
+                Console.WriteLine($"Odd Count: {odd.Count}");
+            }
            
 
+            //Drill 05 - Maximum and Minimum Finder
+            Console.WriteLine("====Drill 05 - Maximum and Minimum Finder====");
+            Console.WriteLine("Enter The Number Of Numbers : ");
+            int size;
+            bool isSizeValid = int.TryParse(Console.ReadLine(), out size);
+            if (!isSizeValid || size <= 0)
+            {
+                Console.WriteLine("The Count Must Be Greater Than 0");
+            }
+            else
+            {
+                List<int> numbers = new List<int>();
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine($"Enter Number{i + 1}");
+                    int num;
+                    bool isNumberValid = int.TryParse(Console.ReadLine(), out num);
+                    if (!isNumberValid)
+                    {
+                        Console.WriteLine("Invalid Number");
+                        i--;
+                        continue;
+                    }
+                    numbers.Add(num);
+                }
+                int max = numbers[0];
+                int min = numbers[0];
+                foreach (int i in numbers)
+                {
+                    if (i > max)
+                    {
+                        max = i;
+                    }
+                    else if (i < min)
+                    {
+                        min = i;
+                    }
+                   
+                }
+                Console.WriteLine($"Max {max} || Min {min}");
+            }
+
+
+
+            // Drill 06 — Word Counter
+            Console.WriteLine("====Drill 06 — Word Counter====");
+
+            Console.WriteLine("Enter A Sentence : ");
+            string sentence = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(sentence))
+            {
+                Console.WriteLine("The Sentence Cannot Be Empty");
+            }
+            else
+            {
+                sentence = sentence.Trim();
+
+                string[] words = sentence.Split(' ',
+                    StringSplitOptions.RemoveEmptyEntries
+                );
+
+                Console.WriteLine($"Word Count: {words.Length}");
+            }
+
+
         }
+
+
+
+
     }
 }
