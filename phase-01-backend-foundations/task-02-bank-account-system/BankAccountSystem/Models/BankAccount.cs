6b46c26d16ce617d212f4172050ab408a02c84fb
shahd mohamed <shahddmohamed55@gmail.com>
2026-08-14 20:32:13 +0300

    add commit 1

---
+++ phase-01-backend-foundations/task-02-bank-account-system/BankAccountSystem/Models/BankAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Task02.BankAccountSystem.Models;
+
+namespace Task02.BankAccountSystem.Models
+{
+    public class BankAccount
+    {
+        public string AccountNumber { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public decimal Balance { get; private set; }
+
+        public AccountType AccountType { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+        public BankAccount(decimal initialBalance)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Initial balance cannot be negative.");
+            }
+
+            Balance = initialBalance;
+        }
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive.");
+            }
+
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdraw amount must be positive.");
+            }
+
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Insufficient balance.");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ phase-01-backend-foundations/task-02-bank-account-system/BankAccountSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Security.Principal;
+using Task02.BankAccountSystem.Services;
+
+namespace Task02.BankAccountSystem
+{
+    public class Program
+    {
+       
+    }
+}
