5e1acbe36f978be075e91754b68540b4dafae73b
shahd mohamed <shahddmohamed55@gmail.com>
2026-08-14 18:47:28 +0300

    Add Task 02 Bank Account System

---
+++ phase-01-backend-foundations/task-02-bank-account-system/BankAccountSystem/Models/TransactionType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02.BankAccountSystem.Models
+{
+    public class TransactionType
+    {
+        public enum TransactionTypes
+        {
+            Deposit,
+            Withdraw,
+            TransferOut,
+            TransferIn
+        }
+    }
+}
